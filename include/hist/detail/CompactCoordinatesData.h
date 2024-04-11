a14348ebabe0cddc976ab1cf440394c16e9879c5
klytje <k.relle@live.dk>
2024-04-11 12:47:58 +0200

    fixed warnings on linux

--- include/hist/detail/CompactCoordinatesData.h
+++ include/hist/detail/CompactCoordinatesData.h
@@ -7,8 +7,12 @@
 
 // AVX implies SSE4.1 and SSE2, but the MSVC compiler doesn't seem to define the latter two
 #if defined __AVX__
-    #define __SSE2__
-    #define __SSE4_1__
+    #if !defined __SSE2__
+        #define __SSE2__
+    #endif
+    #if !defined __SSE4_1__
+        #define __SSE4_1__
+    #endif
 #endif
 
 namespace hist::detail {
