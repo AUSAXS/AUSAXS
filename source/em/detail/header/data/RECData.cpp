c5fac580acd80af48f078943aac86629bcd54d8b
krellemeister <k.relle@live.dk>
2024-04-30 10:28:39 +0200

    updated licence

--- source/em/detail/header/data/RECData.cpp
+++ source/em/detail/header/data/RECData.cpp
@@ -1,5 +1,5 @@
 /*
-This software is distributed under the GNU General Public License v3.0. 
+This software is distributed under the GNU Lesser General Public License v3.0. 
 For more information, please refer to the LICENSE file in the project root.
 */
 
