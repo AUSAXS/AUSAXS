4eee5b8b5db524b8963218997a4ab097ed34787a
krellemeister <k.relle@live.dk>
2024-05-11 13:02:55 +0200

    update

--- executable/exv_comparison.cpp
+++ executable/exv_comparison.cpp
@@ -4,11 +4,7 @@
 #include <data/Body.h>
 #include <data/record/Atom.h>
 #include <data/record/Water.h>
-<<<<<<< HEAD
 #include <grid/Grid.h>
-=======
-#include <hydrate/Grid.h>
->>>>>>> 0f6f8910 (analysis)
 #include <hist/distance_calculator/HistogramManagerMT.h>
 #include <hist/distance_calculator/HistogramManagerMTFFGrid.h>
 #include <hist/distance_calculator/HistogramManagerMTFFAvg.h>
