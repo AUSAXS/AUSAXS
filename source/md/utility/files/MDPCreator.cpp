6c97317daa7c1236e8b79de9ac66e4aeabf49fa0
krellemeister <k.relle@live.dk>
2024-06-10 14:06:09 +0200

    added licence to md files

--- source/md/utility/files/MDPCreator.cpp
+++ source/md/utility/files/MDPCreator.cpp
@@ -1,3 +1,8 @@
+/*
+This software is distributed under the GNU Lesser General Public License v3.0. 
+For more information, please refer to the LICENSE file in the project root.
+*/
+
 #include <md/utility/files/MDPCreator.h>
 #include <md/utility/Exceptions.h>
 
