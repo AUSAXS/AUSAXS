081cea612b4fc66b441c7be0cb0a7ddd9e134783
krellemeister <k.relle@live.dk>
2024-06-13 13:55:27 +0200

    removed unsafe OverlapConstraint constructor

--- source/rigidbody/constraints/OverlapConstraint.cpp
+++ source/rigidbody/constraints/OverlapConstraint.cpp
@@ -12,8 +12,6 @@ For more information, please refer to the LICENSE file in the project root.
 
 using namespace rigidbody::constraints;
 
-OverlapConstraint::OverlapConstraint() = default;
-
 OverlapConstraint::OverlapConstraint(data::Molecule* protein) {
     this->protein = protein;
     initialize();
