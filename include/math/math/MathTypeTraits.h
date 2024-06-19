3085ca04230a81af8196b846f9296c28221ddb3a
krellemeister <k.relle@live.dk>
2024-06-19 10:42:48 +0200

    fixed msvc static_asserts

--- include/math/math/MathTypeTraits.h
+++ include/math/math/MathTypeTraits.h
@@ -3,11 +3,16 @@
 #include <type_traits>
 
 namespace detail {
-    template<class P>
-    struct M {
-        operator P const&();
-        operator P&&();
-    };
-    template<class T> constexpr bool is_move_constructible_v = std::is_nothrow_move_constructible_v<T> && !std::is_constructible_v<T, M<T>>;
+    #if defined(_MSC_VER)
+        template<class T> constexpr bool is_move_constructible_v = std::is_nothrow_move_constructible_v<T>;
+    #else
+        // taken from https://stackoverflow.com/a/51912859
+        template<class P>
+        struct M {
+            operator P const&();
+            operator P&&();
+        };
+        template<class T> constexpr bool is_move_constructible_v = std::is_nothrow_move_constructible_v<T> && !std::is_constructible_v<T, M<T>>;
+    #endif
 }
 template<class T> constexpr bool supports_nothrow_move_v = detail::is_move_constructible_v<T> && std::is_nothrow_move_assignable_v<T>;
