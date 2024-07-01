c7fc1cb4da77bafcaf845346d8c9953e9af4a82b
krellemeister <k.relle@live.dk>
2024-07-01 14:11:08 +0200

    updated version

--- include/core/constants/Version.h
+++ include/core/constants/Version.h
@@ -1,9 +1,9 @@
 #pragma once
 
 namespace constants {
-    inline constexpr char version[] = "v1.0.0";
+    inline constexpr char version[] = "v1.0.3";
     inline constexpr char licence[] = 
-        "Copyright (C) 2024 Kristian Lytje\n"
+        "Copyright (C) 2024 Kristian Lytje & Jan Skov Pedersen.\n"
         "This program comes with ABSOLUTELY NO WARRANTY.\n"
         "This is free software, and you are welcome to redistribute it under certain conditions.\n"
         "For details see the LICENCE file.\n";
