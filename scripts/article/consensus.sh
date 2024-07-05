56a9b424ce182f169d85eeeac669fc3192f3702e
krellemeister <k.relle@live.dk>
2024-07-05 12:37:48 +0200

    update

--- scripts/article/consensus.sh
+++ scripts/article/consensus.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 files=(
+	data/consensus/SASDPP4
+	data/consensus/SASDPQ4
+	data/consensus/SASDPR4
 	data/consensus/SASDPS4
+	data/consensus/SASDPT4
 )
 
 size=${#files[@]}
@@ -13,46 +17,49 @@ for ((i=0; i<${size}; i++)); do
 	build/bin/remove_fourth_column_data ${base}.dat
 
 	# ausaxs
-	build/bin/fit_all_exv ${base}_stripped.pdb ${base}_stripped.dat --no-exit-on-unknown-atom -o output/consensus/
-	build/bin/fit_all_exv ${base}_WAXSiS_stripped.pdb ${base}_stripped.dat --no-exit-on-unknown-atom -o output/consensus/
-	mkdir -p output/saxs_fitter/${stem}
-	mkdir -p output/saxs_fitter/${stem}_WAXSiS
+	# cp ${base}_stripped.dat ${base}_WAXSiS_stripped.dat
+	# build/bin/fit_all_exv ${base}_stripped.pdb ${base}_stripped.dat --no-exit-on-unknown-atom -o output/consensus/
+	# build/bin/fit_all_exv ${base}_WAXSiS_stripped.pdb ${base}_WAXSiS_stripped.dat --no-exit-on-unknown-atom -o output/consensus/
+	# mkdir -p output/saxs_fitter/${stem}
+	# mkdir -p output/saxs_fitter/${stem}_WAXSiS
 
-	# foxs
+	# # foxs
 	rm -rf temp/foxs
 	mkdir -p temp/foxs
-	cp ${base}_stripped.pdb ${base}_WAXSiS_stripped.pdb ${base}_stripped.dat temp/foxs
+	cp ${base}_stripped.pdb ${base}_WAXSiS_stripped.pdb ${base}_stripped.dat ${base}_WAXSiS_stripped.dat temp/foxs
 	cd temp/foxs
-	foxs $(basename ${base})_stripped.pdb $(basename ${base})_stripped.dat --max_q=1
-	mv temp/foxs/*.fit output/consensus/${stem}/foxs.fit
-	foxs $(basename ${base})_WAXSiS_stripped.pdb $(basename ${base})_WAXSiS_stripped.dat --max_q=1
-	mv temp/foxs/*.fit output/consensus/${stem}_WAXSiS/foxs.fit
+	foxs ${stem}_stripped.dat ${stem}_stripped.pdb --max_q=1
 	cd ../..
+	mv temp/foxs/*.fit output/consensus/${stem}/foxs.fit
+	cd temp/foxs
+	foxs ${stem}_WAXSiS_stripped.dat ${stem}_WAXSiS_stripped.pdb --max_q=1
+	cd ../..
+	mv temp/foxs/*.fit output/consensus/${stem}_WAXSiS/foxs.fit
 
-	# pepsi
-	rm -rf temp/pepsi
-	mkdir -p temp/pepsi
-	~/tools/Pepsi-SAXS/Pepsi-SAXS ${base}_stripped.pdb ${base}_stripped.dat -o "temp/pepsi/pepsi.fit" -ms 1
-	mv temp/pepsi/pepsi.fit output/consensus/${stem}/pepsi.fit
-	~/tools/Pepsi-SAXS/Pepsi-SAXS ${base}_WAXSiS_stripped.pdb ${base}_stripped.dat -o "temp/pepsi/pepsi.fit" -ms 1
-	mv temp/pepsi/pepsi.fit output/consensus/${stem}_WAXSiS/pepsi.fit
+	# # pepsi
+	# rm -rf temp/pepsi
+	# mkdir -p temp/pepsi
+	# ~/tools/Pepsi-SAXS/Pepsi-SAXS ${base}_stripped.pdb ${base}_stripped.dat -o "temp/pepsi/pepsi.fit" -ms 1
+	# mv temp/pepsi/pepsi.fit output/consensus/${stem}/pepsi.fit
+	# ~/tools/Pepsi-SAXS/Pepsi-SAXS ${base}_WAXSiS_stripped.pdb ${base}_stripped.dat -o "temp/pepsi/pepsi.fit" -ms 1
+	# mv temp/pepsi/pepsi.fit output/consensus/${stem}_WAXSiS/pepsi.fit
 
-	# crysol
-	mkdir -p temp/crysol
-	crysol_output=$(crysol ${base}_stripped.dat ${base}_stripped.pdb --prefix="temp/crysol/out" --constant --implicit-hydrogen=1 --smax=1)
-    fit_file="output/consensus/${stem}/crysol.fit"
-	mv temp/crysol/out.fit ${fit_file}
-    if grep -q "Chi\^2: ******" "$fit_file"; then
-        chi_square=$(echo "$crysol_output" | grep "Chi-square of fit" | awk -F': ' '{print $2}')
-        sed -i "s/\*\*\*\*\*\*/${chi_square}/" "$fit_file"
-    fi
-	crysol_output=$(crysol ${base}_stripped.dat ${base}_WAXSiS_stripped.pdb --prefix="temp/crysol/out" --constant --implicit-hydrogen=1 --smax=1)
-    fit_file="output/consensus/${stem}_WAXSiS/crysol.fit"
-	mv temp/crysol/out.fit ${fit_file}
-    if grep -q "Chi\^2: ******" "$fit_file"; then
-        chi_square=$(echo "$crysol_output" | grep "Chi-square of fit" | awk -F': ' '{print $2}')
-        sed -i "s/\*\*\*\*\*\*/${chi_square}/" "$fit_file"
-    fi
+	# # crysol
+	# mkdir -p temp/crysol
+	# crysol_output=$(crysol ${base}_stripped.dat ${base}_stripped.pdb --prefix="temp/crysol/out" --constant --implicit-hydrogen=1 --smax=1)
+    # fit_file="output/consensus/${stem}/crysol.fit"
+	# mv temp/crysol/out.fit ${fit_file}
+    # if grep -q "Chi\^2: ******" "$fit_file"; then
+    #     chi_square=$(echo "$crysol_output" | grep "Chi-square of fit" | awk -F': ' '{print $2}')
+    #     sed -i "s/\*\*\*\*\*\*/${chi_square}/" "$fit_file"
+    # fi
+	# crysol_output=$(crysol ${base}_stripped.dat ${base}_WAXSiS_stripped.pdb --prefix="temp/crysol/out" --constant --implicit-hydrogen=1 --smax=1)
+    # fit_file="output/consensus/${stem}_WAXSiS/crysol.fit"
+	# mv temp/crysol/out.fit ${fit_file}
+    # if grep -q "Chi\^2: ******" "$fit_file"; then
+    #     chi_square=$(echo "$crysol_output" | grep "Chi-square of fit" | awk -F': ' '{print $2}')
+    #     sed -i "s/\*\*\*\*\*\*/${chi_square}/" "$fit_file"
+    # fi
 
 	cp output/consensus/${stem}/*.fit output/saxs_fitter/$*
 done
