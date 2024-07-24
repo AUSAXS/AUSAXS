654b8b1e2f0ddce37b2f62b93564d05e4dfb5819
krellemeister <k.relle@live.dk>
2024-07-24 13:57:38 +0200

    added FileExtensions.h for file extension definitions

---
+++ include/core/constants/FileExtensions.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <io/IOFwd.h>
+
+#include <array>
+
+namespace constants::filetypes {
+    namespace detail {
+        template<std::size_t N>
+        class FileType {
+            public:
+                consteval FileType(std::array<const char*, N> extensions) : extensions(std::move(extensions)) {}
+
+                /**
+                * @brief Validate if a file exists and has one of the allowed extensions for this file type.
+                */
+                bool validate(const io::File& path) const;
+
+            private:
+                std::array<const char*, N> extensions;
+        };
+    }
+
+    constexpr detail::FileType structure =        {std::array{".pdb",  ".ent"}};
+    constexpr detail::FileType saxs_data =        {std::array{".dat",  ".rsr",  ".xvg"}};
+    constexpr detail::FileType em_map =           {std::array{".map",  ".ccp4", ".mrc", ".rec"}};
+    constexpr detail::FileType unit_cell =        {std::array{".cell", ".uc"}};
+    constexpr detail::FileType grid =             {std::array{".grid"}};
+    constexpr detail::FileType config =           {std::array{".config", ".conf", ".txt"}};
+    // * remember to specialize cpp file if more than 4 possible extensions are added
+}
