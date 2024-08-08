d8d65d680669e0bb06b100b307fa38428a81214d
krellemeister <k.relle@live.dk>
2024-08-08 20:47:31 +0200

    added test

--- tests/form_factor/precalculated_exv_form_factor_product.cpp
+++ tests/form_factor/precalculated_exv_form_factor_product.cpp
@@ -4,6 +4,7 @@
 #include <form_factor/ExvFormFactor.h>
 #include <form_factor/PrecalculatedFormFactorProduct.h>
 #include <form_factor/PrecalculatedExvFormFactorProduct.h>
+#include <settings/All.h>
 
 using namespace form_factor;
 
@@ -11,8 +12,8 @@ TEST_CASE("PrecalculatedFormFactorProduct::evaluate") {
     SECTION("exv") {
         for (unsigned int ff1 = 0; ff1 < get_count_without_excluded_volume(); ++ff1) {
             for (unsigned int ff2 = 0; ff2 < get_count_without_excluded_volume(); ++ff2) {
-                const ExvFormFactor& ff1_obj = storage::exv::get_form_factor(static_cast<form_factor_t>(ff1));
-                const ExvFormFactor& ff2_obj = storage::exv::get_form_factor(static_cast<form_factor_t>(ff2));
+                const ExvFormFactor& ff1_obj = storage::exv::standard.get_form_factor(static_cast<form_factor_t>(ff1));
+                const ExvFormFactor& ff2_obj = storage::exv::standard.get_form_factor(static_cast<form_factor_t>(ff2));
                 PrecalculatedFormFactorProduct ff(ff1_obj, ff2_obj);
                 for (unsigned int i = 0; i < constants::axes::q_axis.bins; ++i) {
                     REQUIRE_THAT(ff.evaluate(i), Catch::Matchers::WithinRel(ff1_obj.evaluate(constants::axes::q_vals[i])*ff2_obj.evaluate(constants::axes::q_vals[i])));
@@ -25,7 +26,7 @@ TEST_CASE("PrecalculatedFormFactorProduct::evaluate") {
         for (unsigned int ff1 = 0; ff1 < get_count_without_excluded_volume(); ++ff1) {
             for (unsigned int ff2 = 0; ff2 < get_count_without_excluded_volume(); ++ff2) {
                 const FormFactor& ff1_obj = storage::atomic::get_form_factor(static_cast<form_factor_t>(ff1));
-                const ExvFormFactor& ff2_obj = storage::exv::get_form_factor(static_cast<form_factor_t>(ff2));
+                const ExvFormFactor& ff2_obj = storage::exv::standard.get_form_factor(static_cast<form_factor_t>(ff2));
                 PrecalculatedFormFactorProduct ff(ff1_obj, ff2_obj);
                 for (unsigned int i = 0; i < constants::axes::q_axis.bins; ++i) {
                     REQUIRE_THAT(ff.evaluate(i), Catch::Matchers::WithinRel(ff1_obj.evaluate(constants::axes::q_vals[i])*ff2_obj.evaluate(constants::axes::q_vals[i])));
@@ -40,8 +41,8 @@ TEST_CASE("PrecalculatedFormFactorProduct::table") {
         const auto& table = storage::exv::get_precalculated_form_factor_table();
         for (unsigned int ff1 = 0; ff1 < get_count_without_excluded_volume(); ++ff1) {
             for (unsigned int ff2 = 0; ff2 < get_count_without_excluded_volume(); ++ff2) {
-                const ExvFormFactor& ff1_obj = storage::exv::get_form_factor(static_cast<form_factor_t>(ff1));
-                const ExvFormFactor& ff2_obj = storage::exv::get_form_factor(static_cast<form_factor_t>(ff2));
+                const ExvFormFactor& ff1_obj = storage::exv::standard.get_form_factor(static_cast<form_factor_t>(ff1));
+                const ExvFormFactor& ff2_obj = storage::exv::standard.get_form_factor(static_cast<form_factor_t>(ff2));
                 const PrecalculatedFormFactorProduct& ff = table.index(ff1, ff2);
                 for (unsigned int i = 0; i < constants::axes::q_axis.bins; ++i) {
                     REQUIRE_THAT(ff.evaluate(i), Catch::Matchers::WithinRel(ff1_obj.evaluate(constants::axes::q_vals[i])*ff2_obj.evaluate(constants::axes::q_vals[i])));
@@ -55,12 +56,76 @@ TEST_CASE("PrecalculatedFormFactorProduct::table") {
         for (unsigned int ff1 = 0; ff1 < get_count_without_excluded_volume(); ++ff1) {
             for (unsigned int ff2 = 0; ff2 < get_count_without_excluded_volume(); ++ff2) {
                 const FormFactor& ff1_obj = storage::atomic::get_form_factor(static_cast<form_factor_t>(ff1));
-                const ExvFormFactor& ff2_obj = storage::exv::get_form_factor(static_cast<form_factor_t>(ff2));
+                const ExvFormFactor& ff2_obj = storage::exv::standard.get_form_factor(static_cast<form_factor_t>(ff2));
                 const PrecalculatedFormFactorProduct& ff = table.index(ff1, ff2);
                 for (unsigned int i = 0; i < constants::axes::q_axis.bins; ++i) {
                     REQUIRE_THAT(ff.evaluate(i), Catch::Matchers::WithinRel(ff1_obj.evaluate(constants::axes::q_vals[i])*ff2_obj.evaluate(constants::axes::q_vals[i])));
                 }
             }
         }
+    }
+}
+
+TEST_CASE("ExvFormFactor: switch volumes") {
+    auto test = [] (const constants::displaced_volume::detail::DisplacedVolumeSet& vols) {
+        SECTION("exv") {
+            const auto& table = storage::exv::get_precalculated_form_factor_table();
+            auto ffset = form_factor::detail::ExvFormFactorSet(vols);
+            for (unsigned int ff1 = 0; ff1 < get_count_without_excluded_volume(); ++ff1) {
+                for (unsigned int ff2 = 0; ff2 < get_count_without_excluded_volume(); ++ff2) {
+                    const ExvFormFactor& ff1_obj = ffset.get_form_factor(static_cast<form_factor_t>(ff1));
+                    const ExvFormFactor& ff2_obj = ffset.get_form_factor(static_cast<form_factor_t>(ff2));
+                    const PrecalculatedFormFactorProduct& ff = table.index(ff1, ff2);
+                    for (unsigned int i = 0; i < constants::axes::q_axis.bins; ++i) {
+                        REQUIRE_THAT(ff.evaluate(i), Catch::Matchers::WithinRel(ff1_obj.evaluate(constants::axes::q_vals[i])*ff2_obj.evaluate(constants::axes::q_vals[i])));
+                    }
+                }
+            }
+        }
+
+        SECTION("cross") {
+            const auto& table = storage::cross::get_precalculated_form_factor_table();
+            auto ffset = form_factor::detail::ExvFormFactorSet(vols);
+            for (unsigned int ff1 = 0; ff1 < get_count_without_excluded_volume(); ++ff1) {
+                for (unsigned int ff2 = 0; ff2 < get_count_without_excluded_volume(); ++ff2) {
+                    const FormFactor& ff1_obj = storage::atomic::get_form_factor(static_cast<form_factor_t>(ff1));
+                    const ExvFormFactor& ff2_obj = ffset.get_form_factor(static_cast<form_factor_t>(ff2));
+                    const PrecalculatedFormFactorProduct& ff = table.index(ff1, ff2);
+                    for (unsigned int i = 0; i < constants::axes::q_axis.bins; ++i) {
+                        REQUIRE_THAT(ff.evaluate(i), Catch::Matchers::WithinRel(ff1_obj.evaluate(constants::axes::q_vals[i])*ff2_obj.evaluate(constants::axes::q_vals[i])));
+                    }
+                }
+            }
+        }
+    };
+
+    SECTION("Traube") {
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::Traube;
+        test(constants::displaced_volume::Traube);
+    }
+
+    SECTION("Voronoi_explicit_H") {
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::Voronoi_explicit_H;
+        test(constants::displaced_volume::Voronoi_explicit_H);
+    }
+
+    SECTION("Voronoi_implicit_H") {
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::Voronoi_implicit_H;
+        test(constants::displaced_volume::Voronoi_implicit_H);
+    }
+
+    SECTION("MinimumFluctutation_explicit_H") {
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::MinimumFluctutation_explicit_H;
+        test(constants::displaced_volume::Voronoi_explicit_H);
+    }
+
+    SECTION("MinimumFluctutation_implicit_H") {
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::MinimumFluctutation_implicit_H;
+        test(constants::displaced_volume::Voronoi_implicit_H);
+    }
+
+    SECTION("vdw") {
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::vdw;
+        test(constants::displaced_volume::vdw);
     }
 }
