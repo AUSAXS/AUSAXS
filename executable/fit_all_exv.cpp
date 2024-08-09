2603665042c62565ae296eaffdbcf7334a14f636
krellemeister <k.relle@live.dk>
2024-08-09 11:43:06 +0200

    fit all exv update

--- executable/fit_all_exv.cpp
+++ executable/fit_all_exv.cpp
@@ -60,103 +60,100 @@ int main(int argc, char const *argv[]) {
     //######################//
     //### ACTUAL PROGRAM ###//
     //######################//
-    std::vector<settings::hist::HistogramManagerChoice> loop;
-    std::vector<std::string> loop_names;
 
-    std::string volumes = "None";
-    #if TRAUBE_FF
-        loop = {
+    io::Folder(settings::general::output).create();
+    std::ofstream out(settings::general::output + "/ausaxs_chi2.txt");
+    bool printed_volume = false;
+    auto runner = [&] (std::vector<settings::hist::HistogramManagerChoice> loop, std::vector<std::string> loop_names, std::string postfix) {
+        auto perform_fit = [&] (const std::string& name, settings::hist::HistogramManagerChoice choice, bool fit_exv) {
+            settings::hist::histogram_manager = choice;
+
+            data::Molecule protein(pdb);
+            protein.generate_new_hydration();
+            if (!printed_volume) {out << "size: " << std::to_string(protein.size_atom()) << std::endl; printed_volume = true;}
+
+            std::shared_ptr<fitter::HydrationFitter> fitter;
+            if (fit_exv) {fitter = std::make_shared<fitter::ExcludedVolumeFitter>(mfile, protein.get_histogram());}
+            else {fitter = std::make_shared<fitter::HydrationFitter>(mfile, protein.get_histogram());}
+            auto result = fitter->fit();
+            std::cout << name << ": " << result->fval/result->dof << std::endl;
+            out << name << postfix << ": " << result->fval/result->dof << std::endl;
+            // fitter::FitReporter::save(result.get(), settings::general::output + volumes + "/" + name + ".txt");
+        };
+
+        for (unsigned int i = 0; i < loop.size(); ++i) {
+            switch (loop[i]) {
+                case settings::hist::HistogramManagerChoice::HistogramManagerMT:
+                    settings::molecule::use_effective_charge = true;
+                    perform_fit(loop_names[i], loop[i], false);
+                    break;
+                case settings::hist::HistogramManagerChoice::HistogramManagerMTFFGrid:
+                    settings::molecule::use_effective_charge = false;
+                    perform_fit(loop_names[i], loop[i], false);
+                    break;
+                case settings::hist::HistogramManagerChoice::HistogramManagerMTFFGridSurface:
+                    settings::molecule::use_effective_charge = false;
+                    perform_fit(loop_names[i], loop[i], false);
+                    settings::grid::rvol = 2.15;
+                    settings::grid::surface_thickness = 1;
+                    perform_fit(loop_names[i] + "_fitted_215", loop[i], true);
+                    settings::grid::rvol = 3.00;
+                    settings::grid::surface_thickness = 2;
+                    perform_fit(loop_names[i] + "_fitted_300", loop[i], true);
+                    break;
+                case settings::hist::HistogramManagerChoice::HistogramManagerMTFFAvg:
+                case settings::hist::HistogramManagerChoice::HistogramManagerMTFFExplicit:
+                case settings::hist::HistogramManagerChoice::FoXSManager:
+                case settings::hist::HistogramManagerChoice::PepsiManager:
+                case settings::hist::HistogramManagerChoice::CrysolManager:
+                    settings::molecule::use_effective_charge = false;
+                    perform_fit(loop_names[i], loop[i], false);
+                    perform_fit(loop_names[i] + "_fitted", loop[i], true);
+                    break;
+                default:
+                    throw except::invalid_argument("Unknown histogram manager choice.");
+            }
+        }
+    };
+
+    {   // volume independent
+        std::vector<settings::hist::HistogramManagerChoice> loop = {
             settings::hist::HistogramManagerChoice::HistogramManagerMT,
             settings::hist::HistogramManagerChoice::HistogramManagerMTFFGrid,
             settings::hist::HistogramManagerChoice::HistogramManagerMTFFGridSurface,
             settings::hist::HistogramManagerChoice::HistogramManagerMTFFAvg, 
-            settings::hist::HistogramManagerChoice::HistogramManagerMTFFExplicit,
             settings::hist::HistogramManagerChoice::FoXSManager,
             settings::hist::HistogramManagerChoice::CrysolManager,
             settings::hist::HistogramManagerChoice::PepsiManager
         };
-
-        loop_names = {
+        std::vector<std::string> loop_names = {
             "HistogramManagerMT",
             "HistogramManagerMTFFGrid",
             "HistogramManagerMTFFGridSurface",
             "HistogramManagerMTFFAvg", 
-            "HistogramManagerMTFFExplicit",
             "FoXS",
             "CRYSOL",
             "Pepsi-SAXS"
         };
-        volumes = "TRAUBE";
-    #elif PONTIUS_FF
-        loop = {
-            settings::hist::HistogramManagerChoice::HistogramManagerMTFFAvg,
-            settings::hist::HistogramManagerChoice::HistogramManagerMTFFExplicit,
-            settings::hist::HistogramManagerChoice::FoXSManager,
-            settings::hist::HistogramManagerChoice::CrysolManager,
-            settings::hist::HistogramManagerChoice::PepsiManager
-        };
-        loop_names = {
-            "HistogramManagerMTFFAvg",
-            "HistogramManagerMTFFExplicit", 
-            "FoXS",
-            "CRYSOL",
-            "Pepsi-SAXS"
-        };
-        volumes = "PONTIUS";
-    #endif
-    std::cout << volumes << std::endl;
-
-    io::Folder(settings::general::output).create();
-    std::ofstream out(settings::general::output + "/" + volumes + "_" + mfile.stem() + ".txt");
-    bool printed_volume = false;
-    auto perform_fit = [&] (const std::string& name, settings::hist::HistogramManagerChoice choice, bool fit_exv) {
-        settings::hist::histogram_manager = choice;
-
-        data::Molecule protein(pdb);
-        protein.generate_new_hydration();
-        if (!printed_volume) {out << "size: " << std::to_string(protein.size_atom()) << std::endl; printed_volume = true;}
-
-        std::shared_ptr<fitter::HydrationFitter> fitter;
-        if (fit_exv) {fitter = std::make_shared<fitter::ExcludedVolumeFitter>(mfile, protein.get_histogram());}
-        else {fitter = std::make_shared<fitter::HydrationFitter>(mfile, protein.get_histogram());}
-        auto result = fitter->fit();
-        std::cout << name << ": " << result->fval/result->dof << std::endl;
-        out << name << ": " << result->fval/result->dof << std::endl;
-        fitter::FitReporter::save(result.get(), settings::general::output + volumes + "/" + name + ".txt");
-    };
-
-    for (unsigned int i = 0; i < loop.size(); ++i) {
-        switch (loop[i]) {
-            case settings::hist::HistogramManagerChoice::HistogramManagerMT:
-                settings::molecule::use_effective_charge = true;
-                perform_fit(loop_names[i], loop[i], false);
-                break;
-            case settings::hist::HistogramManagerChoice::HistogramManagerMTFFGrid:
-                settings::molecule::use_effective_charge = false;
-                perform_fit(loop_names[i], loop[i], false);
-                break;
-            case settings::hist::HistogramManagerChoice::HistogramManagerMTFFGridSurface:
-                settings::molecule::use_effective_charge = false;
-                perform_fit(loop_names[i], loop[i], false);
-                settings::grid::rvol = 2.15;
-                settings::grid::surface_thickness = 1;
-                perform_fit(loop_names[i] + "_fitted_215", loop[i], true);
-                settings::grid::rvol = 3.00;
-                settings::grid::surface_thickness = 2;
-                perform_fit(loop_names[i] + "_fitted_300", loop[i], true);
-                break;
-            case settings::hist::HistogramManagerChoice::HistogramManagerMTFFAvg:
-            case settings::hist::HistogramManagerChoice::HistogramManagerMTFFExplicit:
-            case settings::hist::HistogramManagerChoice::FoXSManager:
-            case settings::hist::HistogramManagerChoice::PepsiManager:
-            case settings::hist::HistogramManagerChoice::CrysolManager:
-                settings::molecule::use_effective_charge = false;
-                perform_fit(loop_names[i], loop[i], false);
-                perform_fit(loop_names[i] + "_fitted", loop[i], true);
-                break;
-            default:
-                throw except::invalid_argument("Unknown histogram manager choice.");
-        }
+        runner(loop, loop_names, "");
     }
-    return 0;
+    {   // dependent
+        std::vector<settings::hist::HistogramManagerChoice> loop = {settings::hist::HistogramManagerChoice::HistogramManagerMTFFExplicit};
+        std::vector<std::string> loop_names = {"HistogramManagerMTFFExplicit"};
+
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::Traube;
+        runner(loop, loop_names, "_traube");
+
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::Voronoi_implicit_H;
+        runner(loop, loop_names, "_voronoi");
+
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::MinimumFluctutation_implicit_H;
+        runner(loop, loop_names, "_mf");
+
+        settings::molecule::displaced_volume_set = settings::molecule::DisplacedVolumeSet::vdw;
+        runner(loop, loop_names, "_vdw");
+    }
+
+
+   return 0;
 }
