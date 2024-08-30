47cb140083ab61565550fcffbfe86f1c20e8fe35
krellemeister <k.relle@live.dk>
2024-08-30 14:12:42 +0200

    plotting script update

--- scripts/plot_vary_cell_width.py
+++ scripts/plot_vary_cell_width.py
@@ -2,24 +2,54 @@ from plot_extensions import load_file
 from plot_helper import *
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+from matplotlib.ticker import FormatStrFormatter
 
-data = load_file("output/vary_grid_cell_size/intensities.png.plot")
+params = {
+    'legend.fontsize': 24,
+    'figure.figsize': (10, 8),
+    'axes.labelsize': 24,
+    'axes.titlesize': 24,
+    'xtick.labelsize': 18,
+    'ytick.labelsize': 18,
+    'lines.markersize': 10,
+    'lines.linewidth': 3
+}
+plt.rcParams.update(params)
 
-fig, ax = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
-plt.sca(ax[0])
-for d in data[:4]:
-    if isinstance(d, Dataset):
-        plot_dataset(d)
+# fig, ax = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
+# plt.sca(ax[0])
+# data = load_file("output/vary_grid_cell_size/intensities.png.plot")
+# for d in data[:4]:
+#     if isinstance(d, Dataset):
+#         plot_dataset(d)
 
-ax[0].set_xscale("linear")
-ax[0].set_ylim(1e-4, 1e-2)
+# ax[0].set_xscale("linear")
+# ax[0].set_ylim(1e-4, 1e-2)
 
-plt.sca(ax[1])
+# plt.sca(ax[1])
+# data = load_file("output/vary_grid_cell_size/difference.png.plot")
+# for d in data[:4]:
+#     if isinstance(d, Dataset):
+#         plot_dataset(d)
+
+# ax[1].set_xscale("linear")
+# ax[1].set_ylim(0.5, 1.05)
+
+plt.figure(figsize=(10, 6))
 data = load_file("output/vary_grid_cell_size/difference.png.plot")
 for d in data[:4]:
     if isinstance(d, Dataset):
         plot_dataset(d)
 
-ax[1].set_xscale("linear")
-ax[1].set_ylim(0.5, 1.05)
+plt.axhline(1, color="black", linestyle="-")
+plt.xscale("linear")
+plt.yscale("log")
+plt.ylabel("Relative difference")
+plt.xlabel("$q\ [\AA^{-1}]$")
+plt.tight_layout()
+plt.gca().yaxis.set_minor_formatter(FormatStrFormatter('%.1f'))
+plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+# plt.ylim(0.5, 1.05)
+plt.savefig("output/vary_grid_cell_size/difference.png", dpi=300)
 plt.show()
