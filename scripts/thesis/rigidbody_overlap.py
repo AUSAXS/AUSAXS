2cf695851419d4eb8096bafccc652528991fd79b
krellemeister <k.relle@live.dk>
2024-10-03 17:37:04 +0200

    update

--- scripts/thesis/rigidbody_overlap.py
+++ scripts/thesis/rigidbody_overlap.py
@@ -4,24 +4,24 @@ import numpy as np
 
 params = {
     'legend.fontsize': 16,
-    'axes.labelsize': 18,
-    'axes.titlesize': 18,
-    'xtick.labelsize': 14,
-    'ytick.labelsize': 14,
+    'axes.labelsize': 20,
+    'axes.titlesize': 20,
+    'xtick.labelsize': 16,
+    'ytick.labelsize': 16,
     'lines.markersize': 10
 }
 plt.rcParams.update(params)
 
-data = np.loadtxt('rigidbody_overlap_1.txt')
+data = np.loadtxt('data/rigidbody_overlap_1.txt')
 
 xx = np.linspace(0, 3, 100)
 data = data[data[:,0] < xx[-1],:]
 data[:,1:] /= np.amax(data[:,1:])
 
 plt.figure(figsize=(10, 6))
-plt.hist(data[:,0], bins=data[:,0], weights=data[:,1], histtype='step', label=r'current conformation')
-plt.hist(data[:,0], bins=data[:,0], weights=data[:,2], histtype='step', label=r'initial conformation')
-plt.plot(xx, np.exp(-5*xx), "k", label='$\exp(-5x)$')
+plt.hist(data[:,0], bins=data[:,0], weights=data[:,1], histtype='step', label=r'current conformation', lw=2)
+plt.hist(data[:,0], bins=data[:,0], weights=data[:,2], histtype='step', label=r'initial conformation', lw=2)
+plt.plot(xx, np.exp(-5*xx), "k", label='$\exp(-5x)$', lw=2)
 plt.xlim([xx[0], xx[-1]])
 plt.ylim([7e-5, 2])
 plt.semilogy()
