dad7a32247a20652767bbadadbaaf1d1b8201315
krellemeister <k.relle@live.dk>
2024-10-16 16:08:47 +0200

    update

--- executable/vary_grid_radii.cpp
+++ executable/vary_grid_radii.cpp
@@ -1,6 +1,3 @@
-#include "fitter/ExcludedVolumeFitter.h"
-#include "fitter/HydrationFitter.h"
-#include "fitter/LinearFitter.h"
 #include <CLI/CLI.hpp>
 
 #include <data/Molecule.h>
@@ -15,11 +12,12 @@
 #include <hist/distance_calculator/HistogramManagerMTFFExplicit.h>
 #include <hist/intensity_calculator/CompositeDistanceHistogramFFGridSurface.h>
 #include <hist/intensity_calculator/ICompositeDistanceHistogramExv.h>
+#include <fitter/ExcludedVolumeFitter.h>
+#include <fitter/HydrationFitter.h>
+#include <fitter/LinearFitter.h>
 #include <dataset/SimpleDataset.h>
 #include <settings/All.h>
 #include <plots/All.h>
-
-#include <array>
 
 int main(int argc, char const *argv[]) {
     if (argc != 3) {
@@ -31,7 +29,8 @@ int main(int argc, char const *argv[]) {
 
     settings::general::output += "vary_grid_radii/" + pdb.stem() + "/";
     settings::molecule::use_effective_charge = false;
-    settings::grid::cell_width = 0.25;
+    settings::grid::cell_width = 0.5;
+    settings::grid::exv::width = settings::grid::cell_width;
     settings::grid::exv::surface_thickness = 1;
 
     data::Molecule molecule(pdb);
@@ -44,11 +43,16 @@ int main(int argc, char const *argv[]) {
     }
     settings::general::verbose = false;
 
-    std::vector<Dataset> datasets, datasets_hydro_fitted, datasets_exv_fitted, profile_exv;
-    std::vector<double> chi2_hydro, chi2_exv;
-    for (double r = 1; r <= 3; r += 0.1) {
+    std::vector<Dataset> datasets, datasets_hydro_fitted, profile_exv;
+    std::vector<double> chi2_hydro, volumes, dummy_atoms;
+    for (double r = 1.5; r <= 3; r += 0.1) {
+        molecule.clear_grid();
         std::cout << "radius: " << r << std::endl;
         settings::grid::min_exv_radius = r;
+
+        volumes.push_back(molecule.get_volume_grid());
+        dummy_atoms.push_back(molecule.get_grid()->generate_excluded_volume(false).interior.size());
+
         auto hist = hist::HistogramManagerMTFFGridSurface(molecule).calculate_all();
         auto hist_cast = static_cast<hist::ICompositeDistanceHistogramExv*>(hist.get());
         datasets.push_back(hist->debye_transform().as_dataset());
@@ -59,19 +63,15 @@ int main(int argc, char const *argv[]) {
         datasets_hydro_fitted.push_back(fit->info.fitted_intensity_interpolated);
         chi2_hydro.push_back(fit->fval/fit->dof);
 
-        fitter::ExcludedVolumeFitter fitter_exv(saxs, hist::HistogramManagerMTFFGridSurface(molecule).calculate_all());
-        auto fit_exv = fitter_exv.fit();
-        datasets_exv_fitted.push_back(fit_exv->info.fitted_intensity_interpolated);
-        chi2_exv.push_back(fit_exv->fval/fit_exv->dof);
-
         // molecule.get_grid()->save(settings::general::output + "grid_" + std::to_string(r) + ".pdb");
-        molecule.clear_grid();
     }
 
     style::color_map::Rainbow cmap(datasets.size());
-    plots::PlotDataset plot, plot_hydro, plot_exv, plot_relative, plot_aa_xx;
+    plots::PlotDataset plot, plot_hydro, plot_relative, plot_aa_xx;
+    std::vector<double> indices(datasets.size());
     for (unsigned int i = 0; i < datasets.size(); i++) {
-        double r = 1 + 0.1 * i;
+        indices[i] = i;
+        double r = 1.5 + 0.1 * i;
         auto c = cmap.next();
         plot.plot(
             datasets[i], 
@@ -88,20 +88,12 @@ int main(int argc, char const *argv[]) {
                 {"title", "Fitted hydration"}}
             )
         );
-        plot_exv.plot(
-            datasets_exv_fitted[i], 
-            plots::PlotOptions(
-                {{"xlabel", "q"}, {"ylabel", "Intensity"}, {"yrange", std::vector{1e-4, 1e2}}, {"normalize", true}, 
-                {"logx", true}, {"logy", true}, {"color", c}, {"normalize", true}, {"legend", std::to_string(chi2_exv[i])},
-                {"title", "Fitted exv and hydration"}}
-            )
-        );
 
         SimpleDataset relative = standard;
-        SimpleDataset aa_xx = profile_atomic;
+        SimpleDataset aa_xx = profile_exv[i];
         for (unsigned int j = 0; j < relative.size(); j++) {
             relative.y(j) = datasets[i].y(j) / standard.y(j);
-            aa_xx.y(j) /= profile_exv[i].y(j);
+            aa_xx.y(j) /= profile_atomic.y(j);
         }
         relative.normalize();
         aa_xx.normalize();
@@ -115,14 +107,23 @@ int main(int argc, char const *argv[]) {
         plot_aa_xx.plot(
             aa_xx, 
             plots::PlotOptions(
-                {{"xlabel", "q"}, {"ylabel", "aa / xx"}, {"yrange", std::vector{0.5, 1.5}}, {"logx", true}, {"color", c}, 
+                {{"xlabel", "q"}, {"ylabel", "xx / aa"}, {"yrange", std::vector{0.5, 1.5}}, {"logx", true}, {"color", c}, 
                 {"normalize", true}, {"title", "Vacuum over exv"}, {"legend", std::to_string(r)}}
             )
         );
     }
     plot.save(settings::general::output + "analysis.png");
     plot_hydro.save(settings::general::output + "analysis_hydro_fitted.png");
-    plot_exv.save(settings::general::output + "analysis_exv_fitted.png");
     plot_aa_xx.save(settings::general::output + "analysis_aa_xx.png");
     plot_relative.save(settings::general::output + "analysis_relative.png");
+    plots::PlotDataset::quick_plot(
+        SimpleDataset{indices, volumes}, 
+        plots::PlotOptions({{"xlabel", "iteration"}, {"ylabel", "volume [Å$^-1$]"}, {"title", "Grid volume"}, {"color", "k"}}), 
+        settings::general::output + "volumes.png"
+    );
+    plots::PlotDataset::quick_plot(
+        SimpleDataset{indices, dummy_atoms}, 
+        plots::PlotOptions({{"xlabel", "iteration"}, {"ylabel", "number of dummy exv atoms"}, {"title", "Exv dummy atoms"}, {"color", "k"}}), 
+        settings::general::output + "exv_atoms.png"
+    );
 }
