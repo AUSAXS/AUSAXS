cc52ca4baecf5a57979cee293349d7ca87a7e2cc
krellemeister <k.relle@live.dk>
2025-01-13 16:52:46 +0100

    converted more methods to header-only

--- include/core/hist/detail/CompactCoordinates.h
+++ include/core/hist/detail/CompactCoordinates.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <hist/detail/CompactCoordinatesData.h>
+#include <data/Body.h>
+#include <constants/Constants.h>
 #include <utility/Concepts.h>
-#include <data/DataFwd.h>
 
 #include <vector>
 
@@ -62,3 +63,54 @@ namespace ausaxs::hist::detail {
     };
     static_assert(supports_nothrow_move_v<CompactCoordinates>, "CompactCoordinates should support nothrow move semantics.");
 }
+
+//#########################################//
+//############ IMPLEMENTATION #############//
+//#########################################//
+
+// implementation defined in header to support efficient inlining
+
+inline ausaxs::hist::detail::CompactCoordinates::CompactCoordinates(unsigned int size) : data(size) {}
+
+inline ausaxs::hist::detail::CompactCoordinates::CompactCoordinates(std::vector<Vector3<double>>&& coordinates, double weight) : data(coordinates.size()) {
+    std::transform(coordinates.begin(), coordinates.end(), data.begin(), [weight] (const Vector3<double>& v) {return CompactCoordinatesData(v, weight);});
+}
+
+inline ausaxs::hist::detail::CompactCoordinates::CompactCoordinates(const std::vector<data::AtomFF>& atoms) : data(atoms.size()) {
+    for (unsigned int i = 0; i < size(); ++i) {
+        const auto& a = atoms[i]; 
+        data[i] = CompactCoordinatesData(a.coordinates(), a.weight());
+    }
+}
+
+inline ausaxs::hist::detail::CompactCoordinates::CompactCoordinates(const std::vector<data::Body>& bodies) : data(std::accumulate(bodies.begin(), bodies.end(), 0, [](unsigned int sum, const data::Body& body) {return sum + body.size_atom();})) {
+    unsigned int i = 0;
+    for (const auto& body : bodies) {
+        for (const auto& a : body.get_atoms()) {
+            data[i++] = CompactCoordinatesData(a.coordinates(), a.weight());
+        }
+    }
+}
+
+inline ausaxs::hist::detail::CompactCoordinates::CompactCoordinates(const std::vector<data::Water>& atoms) : data(atoms.size()) {
+    for (unsigned int i = 0; i < size(); ++i) {
+        const auto& a = atoms[i]; 
+        data[i] = CompactCoordinatesData(a.coordinates(), a.weight());
+    }
+}
+
+inline void ausaxs::hist::detail::CompactCoordinates::implicit_excluded_volume(double volume_per_atom) {
+    double displaced_charge = constants::charge::density::water*volume_per_atom;
+    double charge_per_atom = -displaced_charge;
+    std::for_each(data.begin(), data.end(), [charge_per_atom] (CompactCoordinatesData& d) {d.value.w += charge_per_atom;});
+}
+
+inline std::vector<ausaxs::hist::detail::CompactCoordinatesData>& ausaxs::hist::detail::CompactCoordinates::get_data() {return data;}
+
+inline const std::vector<ausaxs::hist::detail::CompactCoordinatesData>& ausaxs::hist::detail::CompactCoordinates::get_data() const {return data;}
+
+inline std::size_t ausaxs::hist::detail::CompactCoordinates::size() const {return data.size();}
+
+inline ausaxs::hist::detail::CompactCoordinatesData& ausaxs::hist::detail::CompactCoordinates::operator[](unsigned int i) {return data[i];}
+
+inline const ausaxs::hist::detail::CompactCoordinatesData& ausaxs::hist::detail::CompactCoordinates::operator[](unsigned int i) const {return data[i];}
--- include/core/hist/detail/CompactCoordinatesFF.h
+++ include/core/hist/detail/CompactCoordinatesFF.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include <hist/detail/CompactCoordinates.h>
+#include <hist/detail/CompactCoordinatesData.h>
 #include <form_factor/FormFactorType.h>
 #include <utility/Concepts.h>
-#include <data/DataFwd.h>
-#include <math/MathFwd.h>
+#include <data/Body.h>
 
 #include <vector>
 
@@ -42,3 +42,40 @@ namespace ausaxs::hist::detail {
             std::vector<unsigned int> ff_types;
     };
 }
+
+//#########################################//
+//############ IMPLEMENTATION #############//
+//#########################################//
+
+// implementation defined in header to support efficient inlining
+
+inline ausaxs::hist::detail::CompactCoordinatesFF::CompactCoordinatesFF(const data::Body& body) : CompactCoordinates(body.size_atom()), ff_types(body.size_atom()) {
+    for (unsigned int i = 0; i < size(); ++i) {
+        const auto& a = body.get_atom(i); 
+        data[i] = hist::detail::CompactCoordinatesData(a.coordinates(), a.weight());
+        ff_types[i] = static_cast<int>(a.form_factor_type());
+    }
+}
+
+inline ausaxs::hist::detail::CompactCoordinatesFF::CompactCoordinatesFF(const std::vector<data::Body>& bodies) 
+    : CompactCoordinates(std::accumulate(bodies.begin(), bodies.end(), 0, [](unsigned int sum, const data::Body& body) {return sum + body.size_atom();})) 
+{
+    ff_types.resize(size());
+    unsigned int i = 0;
+    for (const auto& body : bodies) {
+        for (const auto& a : body.get_atoms()) {
+            data[i] = hist::detail::CompactCoordinatesData(a.coordinates(), a.weight());
+            ff_types[i++] = static_cast<int>(a.form_factor_type());
+        }
+    }
+}
+
+inline ausaxs::hist::detail::CompactCoordinatesFF::CompactCoordinatesFF(const std::vector<data::Water>& atoms) : CompactCoordinates(atoms.size()), ff_types(atoms.size()) {
+    for (unsigned int i = 0; i < size(); ++i) {
+        const auto& a = atoms[i]; 
+        data[i] = hist::detail::CompactCoordinatesData(a.coordinates(), a.weight());
+        ff_types[i] = static_cast<int>(a.form_factor_type());
+    }
+}
+
+inline unsigned int ausaxs::hist::detail::CompactCoordinatesFF::get_ff_type(unsigned int i) const {return ff_types[i];}
