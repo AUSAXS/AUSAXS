33943537723a9a97992914bc8716c1ad8f6aa145
klytje <kristian.lytje@uni-saarland.de>
2025-04-08 14:48:07 +0200

    fixed vary_exv_radius scripts

--- executable/vary_exv_radius.cpp
+++ executable/vary_exv_radius.cpp
@@ -1,27 +1,20 @@
-#include "fitter/ExcludedVolumeFitter.h"
 #include <CLI/CLI.hpp>
 
 #include <data/Molecule.h>
 #include <data/Body.h>
-#include <data/record/Atom.h>
-#include <data/record/Water.h>
 #include <grid/Grid.h>
-#include <hist/distance_calculator/HistogramManagerMT.h>
-#include <hist/distance_calculator/HistogramManagerMTFFGrid.h>
-#include <hist/distance_calculator/HistogramManagerMTFFAvg.h>
-#include <hist/distance_calculator/HistogramManagerMTFFExplicit.h>
-#include <hist/intensity_calculator/CompositeDistanceHistogramFFGrid.h>
-#include <hist/intensity_calculator/ICompositeDistanceHistogramExv.h>
-#include <fitter/HydrationFitter.h>
+#include <fitter/SmartFitter.h>
 #include <dataset/SimpleDataset.h>
 #include <settings/All.h>
 #include <plots/All.h>
+
+using namespace ausaxs;
 
 class VariableGrid : public grid::Grid {
     public: 
         using grid::Grid::Grid;
 
-		double get_atomic_radius(constants::atom_t) const override {return ra;}
+		double get_atomic_radius(form_factor::form_factor_t) const override {return ra;}
 		double get_hydration_radius() const override {return rh;}
         void set_atomic_radius(double ra) {this->ra = ra;}
         void set_hydration_radius(double rh) {this->rh = rh;}
@@ -34,17 +27,28 @@ int main(int, char const *[]) {
     settings::general::verbose = false;
     settings::general::output = "output/vary_exv_radius/";
     settings::molecule::throw_on_unknown_atom = false;
-    settings::hist::histogram_manager = settings::hist::HistogramManagerChoice::HistogramManagerMTFFExplicit;
-    settings::hist::fit_excluded_volume = true;
+    settings::hist::histogram_manager = settings::hist::HistogramManagerChoice::HistogramManagerMTFFGridSurface;
+    settings::fit::fit_excluded_volume = true;
+    settings::axes::qmax = 1;
     settings::validate_settings();
 
     std::vector<std::pair<std::string, std::string>> input_files = {
+        {"1ubq", "1ubq"},
+        {"6lyz", "6lyz"},
         {"SASDJG5", "SASDJG5"},
-        {"SASDPP4", "consensus/SASDPP4"},
-        {"SASDPQ4", "consensus/SASDPQ4"},
-        {"SASDPR4", "consensus/SASDPR4"},
-        {"SASDPS4", "consensus/SASDPS4"},
-        {"SASDPT4", "consensus/SASDPT4"}
+        {"SASDA92", "SASDA92"},
+        {"SASDEL8", "SASDEL8"},
+        {"SASDFZ3", "SASDFZ3"},
+        {"SASDGD2", "SASDGD2"},
+        {"SASDAW3", "SASDAW3"},
+        {"SASDHP7", "SASDHP7"},
+        {"SASDJQ7", "SASDJQ7"},
+        {"SASDKG4", "SASDKG4"},
+        {"SASDJY3", "SASDJY3"},
+        {"SASDMB5", "rigidbody/SASDMB5"},
+        {"SASDP39", "rigidbody/SASDP39"},
+        {"SASDT85", "SASDT85"},
+        {"SASDTT4", "SASDTT4"}
     };
 
     auto fit_single = [] (const std::pair<std::string, std::string>& input, double r) {
@@ -56,14 +60,9 @@ int main(int, char const *[]) {
         protein.set_grid(std::move(grid));
         protein.generate_new_hydration();
 
-        double chi2;
-        if (settings::hist::fit_excluded_volume) {
-            fitter::ExcludedVolumeFitter fitter({folder + input.first + ".dat"}, protein.get_histogram());
-            chi2 = fitter.fit()->fval/fitter.fit()->dof;
-        } else {
-            fitter::HydrationFitter fitter(folder + input.first + ".dat", protein.get_histogram());
-            chi2 = fitter.fit()->fval/fitter.fit()->dof;
-        }
+        fitter::SmartFitter fitter(SimpleDataset(folder + input.first + ".dat"), protein.get_histogram());
+        auto res = fitter.fit();
+        double chi2 = res->fval / res->dof;
         std::cout << input.first << " " << r << " " << chi2 << std::endl;
         return chi2;
     };
@@ -72,7 +71,7 @@ int main(int, char const *[]) {
     std::ofstream file(settings::general::output + "chi2.txt");
     file << "DATA\t";
     std::vector<std::vector<double>> chi2;
-    for (double r = 1.5; r <= 3.5; r += 0.1) {
+    for (double r = 1.5; r <= 3; r += 0.05) {
         file << r << "\t";
         std::vector<double> chi2_r;
         for (const auto& input : input_files) {
@@ -89,13 +88,10 @@ int main(int, char const *[]) {
         data::Molecule protein(folder + input.first + ".pdb");
         protein.generate_new_hydration();
 
-        if (settings::hist::fit_excluded_volume) {
-            fitter::ExcludedVolumeFitter fitter({folder + input.first + ".dat"}, protein.get_histogram());
-            chi2_default.push_back(fitter.fit()->fval/fitter.fit()->dof);
-        } else {
-            fitter::HydrationFitter fitter(folder + input.first + ".dat", protein.get_histogram());
-            chi2_default.push_back(fitter.fit()->fval/fitter.fit()->dof);
-        }
+        fitter::SmartFitter fitter(SimpleDataset(folder + input.first + ".dat"), protein.get_histogram());
+        auto res = fitter.fit();
+        double chi2 = res->fval / res->dof;
+        chi2_default.push_back(chi2);
     }
     chi2.push_back(chi2_default);
     file << std::endl;
