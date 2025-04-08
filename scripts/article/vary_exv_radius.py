6c733366da14bc7926e556b847c705951fb70106
klytje <kristian.lytje@uni-saarland.de>
2025-04-08 14:48:07 +0200

    fixed vary_exv_radius scripts

--- scripts/article/vary_exv_radius.py
+++ scripts/article/vary_exv_radius.py
@@ -12,7 +12,7 @@ data = data[:, 1:].astype(float)    # remove labels and convert rest to float
 
 # plot exv radii lines with a cmap
 cmap = plt.get_cmap("autumn")
-fig, ax = plt.subplots(figsize=(10, 6))
+fig, ax = plt.subplots(figsize=(20, 12))
 for i in range(data.shape[1]):
     if i == 0 or i == data.shape[1] - 2:
         ax.plot(labels, data[:, i], color=cmap(i / data.shape[1]), label=f"r = {r_vals[i]}")
@@ -24,9 +24,10 @@ for i in range(data.shape[1]):
 ax.set_xlabel("exv radius")
 ax.set_ylabel("Chi2")
 ax.legend()
+plt.savefig("output/vary_exv_radius/chi2_lines.png", dpi=300, bbox_inches="tight")
 
 # make imshow comparison plot
-fig, ax = plt.subplots(figsize=(10, 6))
+fig, ax = plt.subplots(figsize=(20, 12))
 cmap = plt.get_cmap("RdYlGn_r")
 bounds = [-100, -50, -40, -30, -20, -10, -5, -2, -1, 0, 1, 2, 5, 10, 20, 30, 40, 50, 100]
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
@@ -49,4 +50,5 @@ plt.xticks(np.arange(len(r_vals)-1), r_vals[:-1])
 plt.yticks(np.arange(len(labels)), labels, ha="right", rotation_mode="anchor")
 
 plt.colorbar(im, label="Relative improvement [%]", shrink=0.6)
+plt.savefig("output/vary_exv_radius/chi2_imshow.png", dpi=300, bbox_inches="tight")
 plt.show()
