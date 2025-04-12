c0061b3906b54ef5d264286b72d8c503d581c7d6
krellemeister <k.relle@live.dk>
2025-04-12 17:33:01 +0200

    updated other exes

--- executable/md/time_analysis.cpp
+++ executable/md/time_analysis.cpp
@@ -3,6 +3,7 @@
 #include <md/simulate/molecule.h>
 #include <md/simulate/timeanalysis.h>
 #include <io/ExistingFile.h>
+#include <settings/GeneralSettings.h>
 
 #include <CLI/CLI.hpp>
 
@@ -19,47 +20,53 @@ int main(int argc, char const *argv[]) {
     if (!gmx().valid_executable()) {
         throw except::io_error("Gromacs executable not found. Please install Gromacs and add it to your PATH.");
     }
+    settings::general::output += "md/timeanalysis/" + s_pdb.stem() + "/";
 
-    GMXOptions sele {
+    SystemSettings ss {
         .forcefield = option::Forcefield::AMBER99SB_ILDN,
         .watermodel = option::WaterModel::TIP4P2005,
         .boxtype = option::BoxType::DODECAHEDRON,
         .cation = option::Cation::NA,
         .anion = option::Anion::CL,
-
-        .name = s_pdb.stem(),
-        .output = std::string_view("output/time_analysis/" + s_pdb.stem() + "/"),
-        .jobscript = SHFile("scripts/jobscript_slurm_standard.sh").absolute_path(),
-        .setupsim = location::lusi,
-        .mainsim = location::smaug,
-        .bufmdp = std::make_shared<TimeAnalysisMDPCreatorSol>(),
-        .molmdp = std::make_shared<TimeAnalysisMDPCreatorMol>(),
     };
 
     if (io::Folder tmp("temp/md"); !tmp.exists()) {tmp.create();}
-    gmx::gmx::set_logfile(sele.output.str() + "output.log", sele.output.str() + "cmd.log");
+    gmx::gmx::set_logfile(settings::general::output + "output.log", settings::general::output + "cmd.log");
     PDBFile pdb(s_pdb);
 
-    // prepare sims
-    MoleculeOptions mo(sele, pdb);
-    auto molecule = simulate_molecule(mo);
+    auto molecule = simulate_molecule({
+        .system = ss,
+        .jobname = s_pdb.stem() + "_mol",
+        .pdbfile = pdb,
+        .mdp = TimeAnalysisMDPCreatorMol().write(settings::general::output + "mdp/ta_mol.mdp"),
+        .setup_runner = RunLocation::local,
+        .main_runner = RunLocation::local,
+        .jobscript = SHFile("scripts/jobscript_slurm_standard.sh").absolute_path(),
+    });
 
-    SimulateBufferOutput buffer;
-    BufferOptions bo(sele, molecule.gro);
-    buffer = simulate_buffer(bo);
+    auto buffer = simulate_buffer({
+        .system = ss,
+        .jobname = s_pdb.stem() + "_buf",
+        .refgro = molecule.gro,
+        .mdp = TimeAnalysisMDPCreatorSol().write(settings::general::output + "mdp/ta_buf.mdp"),
+        .setup_runner = RunLocation::local,
+        .main_runner = RunLocation::local,
+        .jobscript = SHFile("scripts/jobscript_slurm_standard.sh").absolute_path(),
+    });
 
-    // prepare saxs
-    GMXOptions saxs_sele = sele;
-    saxs_sele.jobscript = SHFile("scripts/jobscript_slurm_swaxs.sh").absolute_path();
-    
-    SAXSOptions so(saxs_sele, std::move(molecule), std::move(buffer), pdb);
-    auto saxs = timeanalysis(so, 500);
+    auto saxs = timeanalysis({
+        .pdbfile = pdb,
+        .molecule = std::move(molecule),
+        .buffer = std::move(buffer),
+        .runner = RunLocation::local,
+        .jobscript = SHFile("scripts/jobscript_slurm_swaxs.sh").absolute_path(),
+    }, 500);
 
     for (int i = 0; i < static_cast<int>(saxs.size()); i++) {saxs[i].job->submit();}
     for (int i = static_cast<int>(saxs.size())-1; i >= 0; i--) { // reverse wait order since the last job is the shortest run
         saxs[i].job->wait();
         auto res = saxs[i].job->result();
-        res.xvg.rename(std::to_string(i+1) + ".xvg").copy({sele.output.str() + "output"});
+        res.xvg.rename(std::to_string(i+1) + ".xvg").copy({settings::general::output + "output"});
     }
     return 0;
 } 
