8fa5341025051820af2dcdbb4dcb2c84251d19fa
klytje <kristian.lytje@uni-saarland.de>
2025-04-15 14:48:44 +0200

    update

--- include/core/settings/MDSettings.h
+++ include/core/settings/MDSettings.h
@@ -3,9 +3,7 @@
 #include <string>
 
 namespace ausaxs::settings::md {
-    extern std::string gmx_path;                    // The path to the GROMACS installation.
-    extern std::string gmx_source_path;             // The path to the GROMACS source installation.
-    extern std::string buffer_path;                 // The path to the buffer directory.
-
-    std::string gmx_top_path();
+    extern std::string gmx_path;        // The path to the GROMACS installation.
+    extern std::string gmx_top_path;    // The path to the GROMACS source installation.
+    extern std::string buffer_path;     // The path to the buffer directory.
 }
