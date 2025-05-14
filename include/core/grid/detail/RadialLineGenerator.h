baef9ede907436798367d1cf1efab5515872db24
krellemeister <k.relle@live.dk>
2025-05-14 12:04:20 +0200

    RadialLineGenerator is no longer static

--- include/core/grid/detail/RadialLineGenerator.h
+++ include/core/grid/detail/RadialLineGenerator.h
@@ -14,20 +14,16 @@ namespace ausaxs::grid::detail {
             RadialLineGenerator(observer_ptr<grid::Grid> grid, std::array<double, 4> radii, int divisions = 8);
             virtual ~RadialLineGenerator();
 
-            /**
-             * @brief Generate the radial lines for the current grid. 
-             *        This will only regenerate the radial lines if the grid width has changed. 
-             */
-            static void generate(std::array<double, 4> radius, int divisions);
-
-        protected:
-            static std::vector<Vector3<int>> rot_bins_1;
-            static std::vector<Vector3<int>> rot_bins_2;
-            static std::vector<Vector3<int>> rot_bins_3;
-            static std::vector<Vector3<int>> rot_bins_4;
-            static std::vector<Vector3<double>> rot_locs_abs;
+            std::vector<Vector3<int>> rot_bins_1;
+            std::vector<Vector3<int>> rot_bins_2;
+            std::vector<Vector3<int>> rot_bins_3;
+            std::vector<Vector3<int>> rot_bins_4;
+            std::vector<Vector3<double>> rot_locs_abs;
 
         private:
-            inline static double width = 0;
+            /**
+             * @brief Generate the radial lines for the current grid. 
+             */
+            void generate(double width, std::array<double, 4> radius, int divisions);
     };
 }
