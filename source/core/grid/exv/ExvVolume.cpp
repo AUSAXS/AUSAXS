0ae8bf5c908bcb6f3502725f42c400f1c89a92a5
krellemeister <k.relle@live.dk>
2025-05-27 19:21:48 +0200

    added missing licence header in cpp files

--- source/core/grid/exv/ExvVolume.cpp
+++ source/core/grid/exv/ExvVolume.cpp
@@ -1,3 +1,8 @@
+/*
+This software is distributed under the GNU Lesser General Public License v3.0. 
+For more information, please refer to the LICENSE file in the project root.
+*/
+
 #include <grid/exv/ExvVolume.h>
 #include <grid/exv/RawGridExv.h>
 #include <grid/exv/RawGridWithSurfaceExv.h>
