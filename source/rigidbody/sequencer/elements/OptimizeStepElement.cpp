110b339bb83c721c8f8c80904cca87b8b2ea8002
krellemeister <k.relle@live.dk>
2025-06-13 18:26:38 +0200

    added caching support to partial histogram managers; should be slightly more efficient now

--- source/rigidbody/sequencer/elements/OptimizeStepElement.cpp
+++ source/rigidbody/sequencer/elements/OptimizeStepElement.cpp
@@ -11,6 +11,7 @@ For more information, please refer to the LICENSE file in the project root.
 #include <rigidbody/RigidBody.h>
 #include <settings/GeneralSettings.h>
 #include <utility/Console.h>
+#include <utility/Logging.h>
 
 #include <iostream>
 
@@ -20,7 +21,9 @@ namespace ausaxs::rigidbody::sequencer {
     OptimizeStepElement::~OptimizeStepElement() = default;
 
     void OptimizeStepElement::run() {
+        logging::log("OptimizeStepElement::run: running optimization step " + std::to_string(LoopElement::global_counter));
         if (owner->_get_sequencer()->_optimize_step()) {
+            logging::log("OptimizeStepElement::run: optimization step " + std::to_string(LoopElement::global_counter) + " accepted");
             if (settings::general::verbose) {
                 std::cout << "Iteration " << LoopElement::global_counter << " of " << LoopElement::total_loop_count << std::endl;
                 console::print_success("\tRigidBody::optimize: Accepted changes. New best chi2: " + std::to_string(owner->_get_best_conf()->chi2));
