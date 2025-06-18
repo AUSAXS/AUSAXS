c5dbb9a55d4ec5c58a5db64c79b8e8615f16fd1f
krellemeister <k.relle@live.dk>
2025-06-18 19:58:47 +0200

    Random is now header-only

--- include/core/utility/Random.h
+++ include/core/utility/Random.h
@@ -3,10 +3,22 @@
 #include <random>
 
 namespace ausaxs::random {
+    namespace detail {
+        inline std::mt19937& get_generator() {
+            static std::mt19937 gen(std::random_device{}());
+            return gen;
+        } 
+    }
+
     /**
      * @brief Set the seed for the random number generator.
      *        A new Mersenne Twister generator with this seed will be initialized and used throughout the library.
      */
-    void set_seed(int seed) noexcept;
-    const std::mt19937& generator();
+    inline void set_seed(int seed) noexcept {
+        detail::get_generator().seed(seed);
+    }
+
+    inline std::mt19937& generator() {
+        return detail::get_generator();
+    }
 }
