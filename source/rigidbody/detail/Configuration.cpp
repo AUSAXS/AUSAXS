337cfa04882b2c34e48d541baae14f9cd1ec8fb3
Krelle <kristian.lytje@uni-saarland.de>
2025-06-20 18:20:33 +0200

    changed Config to store params for each body individually

--- source/rigidbody/detail/Configuration.cpp
+++ source/rigidbody/detail/Configuration.cpp
@@ -3,15 +3,15 @@ This software is distributed under the GNU Lesser General Public License v3.0.
 For more information, please refer to the LICENSE file in the project root.
 */
 
-#include "rigidbody/parameters/Parameter.h"
 #include <rigidbody/detail/Configuration.h>
+#include <rigidbody/Rigidbody.h>
 
 using namespace ausaxs;
 
-rigidbody::detail::Configuration::Configuration() : parameters(), chi2(std::numeric_limits<double>::max()) {}
+rigidbody::detail::Configuration::Configuration() : chi2(std::numeric_limits<double>::max()) {}
 
-rigidbody::detail::Configuration::Configuration(rigidbody::parameter::Parameter&& pars, double chi2) noexcept 
-    : parameters(std::move(pars)), chi2(chi2) 
+rigidbody::detail::Configuration::Configuration(observer_ptr<Rigidbody> rigidbody, double chi2) noexcept 
+    : parameters(rigidbody->molecule.size_body()), chi2(chi2) 
 {}
 
 rigidbody::detail::Configuration::~Configuration() = default;
