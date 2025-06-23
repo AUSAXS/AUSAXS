11e39914cf1e40b271b9ed4b6c19532c8aa56562
Krelle <kristian.lytje@uni-saarland.de>
2025-06-23 16:05:38 +0200

    now logs RNG seed

--- include/core/utility/Random.h
+++ include/core/utility/Random.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <utility/Logging.h>
+
 #include <random>
 
 namespace ausaxs::random {
     namespace detail {
         inline std::mt19937& get_generator() {
-            static std::mt19937 gen(std::random_device{}());
+            static std::mt19937 gen = [](){
+                int seed = std::random_device{}();
+                logging::log("RNG: Initializing random number generator with seed: " + std::to_string(seed));
+                return std::mt19937(seed);
+            }();
             return gen;
         } 
     }
@@ -16,6 +22,7 @@ namespace ausaxs::random {
      */
     inline void set_seed(int seed) noexcept {
         detail::get_generator().seed(seed);
+        logging::log("RNG: Setting random number generator seed to: " + std::to_string(seed));
     }
 
     inline std::mt19937& generator() {
