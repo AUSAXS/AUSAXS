8bdeff376a795fc587883af583c5f1fd47956180
Krelle <kristian.lytje@uni-saarland.de>
2025-06-23 13:41:26 +0200

    seems to be working

--- include/rigidbody/rigidbody/detail/Conformation.h
+++ include/rigidbody/rigidbody/detail/Conformation.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <rigidbody/detail/Configuration.h>
+#include <rigidbody/Rigidbody.h>
+#include <data/Molecule.h>
 #include <data/Body.h>
 
 namespace ausaxs::rigidbody::detail {
@@ -9,8 +11,9 @@ namespace ausaxs::rigidbody::detail {
      *        Storing the original conformation is required to express the current conformation in absolute terms. 
      */
     struct Conformation {
-        Conformation() = default;
-        Conformation(const std::vector<data::Body>& bodies) : configuration(), original_conformation(bodies) {}
+        Conformation();
+        Conformation(observer_ptr<const Rigidbody> rigidbody);
+        ~Conformation();
 
         Configuration configuration;
         std::vector<data::Body> original_conformation;
