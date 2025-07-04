30426b3fc69af94c7351a1911fbc406977cebe5b
Krelle <kristian.lytje@uni-saarland.de>
2025-07-04 16:33:16 +0200

    two-atom submission seems to work

--- executable/gpu_debug.cpp
+++ executable/gpu_debug.cpp
@@ -1,9 +1,15 @@
 #include <webgpu/webgpu.hpp>
 
+#include <vector>
+#include <data/atoms/AtomFF.h>
+
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <thread>
+
+using namespace ausaxs;
+using namespace ausaxs::data;
 
 class WebGPU {
     public:
@@ -11,30 +17,38 @@ class WebGPU {
             initialize();
         }
 
+        void submit_self(const std::vector<Atom>& atoms);
+        void submit_cross(const std::vector<Atom>& atom_1, const std::vector<Atom>& atom_2);
+
         void submit();
+
+        std::vector<float> run();
 
     private:
         wgpu::Instance instance;
         wgpu::Device device;
         wgpu::Adapter adapter;
         wgpu::BindGroupLayout bind_group_layout;
-        wgpu::ComputePipeline compute_pipeline;
+        struct ComputePipelines {
+            wgpu::ComputePipeline self;
+            wgpu::ComputePipeline cross;
+        } pipelines;
         struct BufferData {
             wgpu::Buffer atom_1;
             wgpu::Buffer atom_2;
             wgpu::Buffer histogram;
         } buffers;
-        wgpu::BindGroup bind_group;
+        std::vector<wgpu::Buffer> readback_buffers;
 
         void initialize();
         wgpu::Instance create_instance();
         wgpu::Adapter get_adapter(wgpu::Instance instance);
         wgpu::Device get_device(wgpu::Instance instance);
         wgpu::BindGroupLayout initialize_bind_group_layout(wgpu::Device device);
-        wgpu::ComputePipeline initialize_compute_pipeline(wgpu::Device device, wgpu::BindGroupLayout bind_group_layout);
+        ComputePipelines initialize_compute_pipelines(wgpu::Device device, wgpu::BindGroupLayout bind_group_layout);
         BufferData create_buffers(wgpu::Device device);
         wgpu::BindGroup assign_buffers(wgpu::Device device, wgpu::Buffer buffer1, wgpu::Buffer buffer2, wgpu::Buffer histogram_buffer, wgpu::BindGroupLayout bind_group_layout);
-        void fill_buffers(wgpu::Queue queue, wgpu::Buffer buffer1, wgpu::Buffer buffer2);
+        void test_fill_buffers(wgpu::Queue queue, wgpu::Buffer buffer1, wgpu::Buffer buffer2);
         wgpu::ShaderModule load_shader_module(const std::filesystem::path& path, wgpu::Device device);
 };
 
@@ -42,23 +56,26 @@ void WebGPU::initialize() {
     instance = create_instance();
     device = get_device(instance);
     bind_group_layout = initialize_bind_group_layout(device);
-    compute_pipeline = initialize_compute_pipeline(device, bind_group_layout);
+    pipelines = initialize_compute_pipelines(device, bind_group_layout);
     buffers = create_buffers(device);
-    fill_buffers(device.getQueue(), buffers.atom_1, buffers.atom_2);
-    bind_group = assign_buffers(device, buffers.atom_1, buffers.atom_2, buffers.histogram, bind_group_layout);
 }
 
 void WebGPU::submit() {
     std::cout << "Submitting work..." << std::endl;
     wgpu::CommandEncoder encoder = device.createCommandEncoder();
 
+    // fill buffers with test data
+    test_fill_buffers(device.getQueue(), buffers.atom_1, buffers.atom_2);
+    wgpu::BindGroup bind_group = assign_buffers(device, buffers.atom_1, buffers.atom_2, buffers.histogram, bind_group_layout);
+
     // submit work
     wgpu::ComputePassDescriptor compute_pass_desc = wgpu::Default;
     auto compute_pass = encoder.beginComputePass(compute_pass_desc);
-    compute_pass.setPipeline(compute_pipeline);
+    compute_pass.setPipeline(pipelines.self);
     compute_pass.setBindGroup(0, bind_group, 0, nullptr);
     compute_pass.dispatchWorkgroups(1, 1, 1);
     compute_pass.end();
+    bind_group.release();
 
     // retrieve data
     static std::string readback_buffer_name = "readback buffer";
@@ -82,8 +99,14 @@ void WebGPU::submit() {
     device.getQueue().submit(command_buffer);
     command_buffer.release();
     std::cout << "Command buffer submitted." << std::endl;
+    readback_buffers.push_back(readback_buffer);
+}
 
-    // print
+std::vector<float> WebGPU::run() {
+    assert(readback_buffers.size() == 1);
+    auto readback_buffer = readback_buffers[0];
+    static std::vector<float> result(readback_buffer.getSize());
+
     bool done = false;
     wgpu::BufferMapCallbackInfo map_callback;
     map_callback.mode = wgpu::CallbackMode::AllowProcessEvents;
@@ -93,10 +116,8 @@ void WebGPU::submit() {
         std::cout << "Readback buffer callback!" << std::endl;
         wgpu::Buffer readback_buffer = *reinterpret_cast<wgpu::Buffer*>(p_readback_buffer);
         bool* done = reinterpret_cast<bool*>(p_done);
-        const uint32_t* output = (const uint32_t*) readback_buffer.getConstMappedRange(0, readback_buffer.getSize());
-        for (int i = 0; i < 8; ++i) {
-            std::cout << "output " << i << ": " << output[i] << std::endl;
-        }
+        const float* output = (const float*) readback_buffer.getConstMappedRange(0, readback_buffer.getSize());
+        result.assign(output, output + readback_buffer.getSize());
         *done = true;
         readback_buffer.unmap();
     };
@@ -108,6 +129,9 @@ void WebGPU::submit() {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         instance.processEvents();
     }
+
+    std::for_each(readback_buffers.begin(), readback_buffers.end(), [](wgpu::Buffer& buffer) {buffer.release();});
+    return result;
 }
 
 wgpu::ShaderModule WebGPU::load_shader_module(const std::filesystem::path& path, wgpu::Device device) {
@@ -268,26 +292,29 @@ wgpu::BindGroupLayout WebGPU::initialize_bind_group_layout(wgpu::Device device) 
     return device.createBindGroupLayout(bindgroup_layout);
 }
 
-wgpu::ComputePipeline WebGPU::initialize_compute_pipeline(wgpu::Device device, wgpu::BindGroupLayout bind_group_layout) {
+WebGPU::ComputePipelines WebGPU::initialize_compute_pipelines(wgpu::Device device, wgpu::BindGroupLayout bind_group_layout) {
     wgpu::ShaderModule compute_shader_module = load_shader_module("executable/gpu_debug.wgsl", device);
     std::cout << "Shader module loaded: " << compute_shader_module << std::endl;
 
-    std::cout << "Creating pipeline layout..." << std::endl;
-    wgpu::PipelineLayoutDescriptor pipeline_layout_desc;
-    pipeline_layout_desc.bindGroupLayoutCount = 1;
-    pipeline_layout_desc.bindGroupLayouts = (WGPUBindGroupLayout*) &bind_group_layout;
-    auto pipeline_layout = device.createPipelineLayout(pipeline_layout_desc);
+    auto ctr = [&] (std::string_view shader_name) {
+        std::cout << "Creating compute pipeline for shader: " << shader_name << std::endl;
+        wgpu::PipelineLayoutDescriptor pipeline_layout_desc;
+        pipeline_layout_desc.bindGroupLayoutCount = 1;
+        pipeline_layout_desc.bindGroupLayouts = (WGPUBindGroupLayout*) &bind_group_layout;
+        auto pipeline_layout = device.createPipelineLayout(pipeline_layout_desc);
 
-    std::cout << "Creating pipeline..." << std::endl;
-    std::string entry_point = "calculate_self";
-    wgpu::ComputePipelineDescriptor pipeline_descriptor = wgpu::Default;
-    pipeline_descriptor.compute.entryPoint.data = entry_point.data();
-    pipeline_descriptor.compute.entryPoint.length = entry_point.size();
-    pipeline_descriptor.compute.module = compute_shader_module;
-    pipeline_descriptor.layout = pipeline_layout;
-    wgpu::ComputePipeline pipeline = device.createComputePipeline(pipeline_descriptor);
+        wgpu::ComputePipelineDescriptor pipeline_descriptor = wgpu::Default;
+        pipeline_descriptor.compute.entryPoint.data = shader_name.data();
+        pipeline_descriptor.compute.entryPoint.length = shader_name.size();
+        pipeline_descriptor.compute.module = compute_shader_module;
+        pipeline_descriptor.layout = pipeline_layout;
+        return device.createComputePipeline(pipeline_descriptor);
+    };
 
-    return pipeline;
+    return {
+        .self=ctr("calculate_self"),
+        .cross=ctr("calculate_cross")
+    };
 }
 
 WebGPU::BufferData WebGPU::create_buffers(wgpu::Device device) {
@@ -361,7 +388,7 @@ wgpu::BindGroup WebGPU::assign_buffers(wgpu::Device device, wgpu::Buffer buffer1
     return device.createBindGroup(bind_group_desc);
 }
 
-void WebGPU::fill_buffers(wgpu::Queue queue, wgpu::Buffer buffer1, wgpu::Buffer buffer2) {
+void WebGPU::test_fill_buffers(wgpu::Queue queue, wgpu::Buffer buffer1, wgpu::Buffer buffer2) {
     std::cout << "Filling buffers..." << std::endl;
     std::array<float, 8> data1 = {0, 0, 1, 2, 0, 0, 2, 4};
     std::array<float, 8> data2 = {1, 0, 1, 6, 1, 0, 2, 8};
@@ -369,8 +396,81 @@ void WebGPU::fill_buffers(wgpu::Queue queue, wgpu::Buffer buffer1, wgpu::Buffer 
     queue.writeBuffer(buffer2, 0, data2.data(), buffer2.getSize());
 }
 
+std::vector<float> convert_data(const std::vector<Atom>& atoms) {
+    std::vector<float> data(atoms.size()*4);
+    for (int i = 0; i < static_cast<int>(atoms.size()); ++i) {
+        const auto& atom = atoms[i];
+        data[i*4 + 0] = atom.x();
+        data[i*4 + 1] = atom.y();
+        data[i*4 + 2] = atom.z();
+        data[i*4 + 3] = atom.w;
+    }
+    return data;
+}
+
+void WebGPU::submit_self(const std::vector<Atom>& atoms) {
+    std::cout << "Calculating self..." << std::endl;
+    wgpu::CommandEncoder encoder = device.createCommandEncoder();
+    wgpu::Queue queue = device.getQueue();
+
+    {   // write data to buffers
+        auto data = convert_data(atoms);
+        assert(data.size() < buffers.atom_1.getSize() && "Data size exceeds buffer size.");
+        queue.writeBuffer(buffers.atom_1, 0, data.data(), buffers.atom_1.getSize());
+    }
+    wgpu::BindGroup bind_group = assign_buffers(device, buffers.atom_1, buffers.atom_2, buffers.histogram, bind_group_layout);
+
+    // submit work
+    wgpu::ComputePassDescriptor compute_pass_desc = wgpu::Default;
+    auto compute_pass = encoder.beginComputePass(compute_pass_desc);
+    compute_pass.setPipeline(pipelines.self);
+    compute_pass.setBindGroup(0, bind_group, 0, nullptr);
+    compute_pass.dispatchWorkgroups(1, 1, 1);
+    compute_pass.end();
+
+    // retrieve data
+    static std::string readback_buffer_name = "readback buffer";
+    wgpu::BufferDescriptor readback_buffer_desc;
+    readback_buffer_desc.label.data = readback_buffer_name.data();
+    readback_buffer_desc.label.length = readback_buffer_name.size();
+    readback_buffer_desc.size = buffers.histogram.getSize();
+    readback_buffer_desc.mappedAtCreation = false;
+    readback_buffer_desc.usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::MapRead;
+    wgpu::Buffer readback_buffer = device.createBuffer(readback_buffer_desc);
+    assert(readback_buffer.getSize() == buffers.histogram.getSize() && "Readback buffer size does not match histogram buffer size.");
+    encoder.copyBufferToBuffer(buffers.histogram, 0, readback_buffer, 0, readback_buffer.getSize());
+
+    wgpu::CommandBufferDescriptor command_buffer_desc;
+    command_buffer_desc.nextInChain = nullptr;
+    command_buffer_desc.label.data = "command buffer";
+    auto command_buffer = encoder.finish(command_buffer_desc);
+    encoder.release();
+
+    std::cout << "Submitting command buffer..." << std::endl;
+    device.getQueue().submit(command_buffer);
+    command_buffer.release();
+    std::cout << "Command buffer submitted." << std::endl;
+    readback_buffers.push_back(readback_buffer);    
+}
+
+void WebGPU::submit_cross(const std::vector<Atom>& atom_1, const std::vector<Atom>& atom_2) {
+    std::cout << "Calculating self..." << std::endl;
+}
+
 int main(int, char const *[]) {
+    std::vector<Atom> atoms = {
+        Atom({0, 0, 0}, 1), 
+        Atom({0, 0, 1}, 1)
+    };
+
     WebGPU webgpu;
-    webgpu.submit();
+    webgpu.submit_self(atoms);
+    auto res = webgpu.run();
+
+    std::cout << "Results:" << std::endl;
+    for (int i = 0; i < 8; ++i) {
+        std::cout << res[i] << std::endl;
+    }
+
     return 0;
 }
