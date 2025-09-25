73aec276274b43c3aa1e0f0ae79b6d43d192c64f
Krelle <kristian.lytje@uni-saarland.de>
2025-09-25 16:27:10 +0200

    added missing licences

--- source/rigidbody/controller/ControllerFactory.cpp
+++ source/rigidbody/controller/ControllerFactory.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Author: Kristian Lytje
+
 #include <rigidbody/controller/ControllerFactory.h>
 #include <rigidbody/controller/SimpleController.h>
 #include <utility/Exceptions.h>
