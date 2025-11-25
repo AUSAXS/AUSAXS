d1122eca58b840650d11589b9b59d9e8824194fa
krellemeister <k.relle@live.dk>
2025-11-25 12:54:12 +0100

    wip

--- include/core/hist/detail/CompactCoordinates.h
+++ include/core/hist/detail/CompactCoordinates.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <hist/detail/CompactCoordinatesTemplate.h>
+#include <utility/Concepts.h>
 
 namespace ausaxs::hist::detail {
     template<bool variable_bin_width>
@@ -12,17 +13,6 @@ namespace ausaxs::hist::detail {
         }
     };
 
-    template<bool variable_bin_width>
-    struct CompactCoordinatesFF : public CompactCoordinatesTemplate<CoordinateTypeXYZFF, variable_bin_width> {
-        using CompactCoordinatesTemplate<CoordinateTypeXYZFF, variable_bin_width>::CompactCoordinatesTemplate;
-
-        int32_t get_ff_type(unsigned int i) const {
-            return this->get_non_coordinate_value(i);
-        }
-    };
-
     static_assert(supports_nothrow_move_v<CompactCoordinates<true>>,    "CompactCoordinates should support nothrow move semantics.");
     static_assert(supports_nothrow_move_v<CompactCoordinates<false>>,   "CompactCoordinates should support nothrow move semantics.");
-    static_assert(supports_nothrow_move_v<CompactCoordinatesFF<true>>,  "CompactCoordinatesFF should support nothrow move semantics.");
-    static_assert(supports_nothrow_move_v<CompactCoordinatesFF<false>>, "CompactCoordinatesFF should support nothrow move semantics.");
 }
---
+++ include/core/hist/detail/CompactCoordinatesFF.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <hist/detail/CompactCoordinatesTemplate.h>
+#include <utility/Concepts.h>
+
+namespace ausaxs::hist::detail {
+    template<bool variable_bin_width>
+    struct CompactCoordinatesFF : public CompactCoordinatesTemplate<CoordinateTypeXYZFF, variable_bin_width> {
+        using CompactCoordinatesTemplate<CoordinateTypeXYZFF, variable_bin_width>::CompactCoordinatesTemplate;
+
+        int32_t get_ff_type(unsigned int i) const {
+            return this->get_non_coordinate_value(i);
+        }
+
+        
+    };
+
+    static_assert(supports_nothrow_move_v<CompactCoordinatesFF<true>>,  "CompactCoordinatesFF should support nothrow move semantics.");
+    static_assert(supports_nothrow_move_v<CompactCoordinatesFF<false>>, "CompactCoordinatesFF should support nothrow move semantics.");
+}
