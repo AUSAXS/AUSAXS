d57919d10ccdbaf0f6c92fb5df8092b0ff1b6371
Krelle <kristian.lytje@uni-saarland.de>
2026-01-14 22:22:46 +0100

    removed all crystal files

--- include/crystal/crystal/CrystalFwd.h
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
-// Author: Kristian Lytje
-
-#pragma once
-
-namespace ausaxs::crystal {
-	struct MillerGenerationStrategy;
-	class Fval;
-}
--- include/crystal/crystal/io/CrystalIOFwd.h
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
-// Author: Kristian Lytje
-
-#pragma once
-
-namespace ausaxs::crystal::io { struct CrystalReader; }
