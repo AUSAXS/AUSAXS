f754180b4f0c5a7e4bffba90a6fb8e582c74a412
Kristian <kristian.lytje@uni-saarland.de>
2026-02-11 18:35:47 +0100

    removed broken include

--- include/api/api/api_pyausaxs.h
+++ include/api/api/api_pyausaxs.h
@@ -8,23 +8,4 @@
 #include <api/pyausaxs/api_iterative_fit.h>
 #include <api/pyausaxs/api_molecule.h>
 #include <api/pyausaxs/api_pdb.h>
-#include <api/pyausaxs/api_rigidbody.h>
 #include <api/pyausaxs/api_settings.h>
-
-// extern "C" API int map_read(
-//     const char* filename,
-//     int* status
-// );
-
-// extern "C" API void map_get_slice(
-//     int map_id,
-//     double z_position,
-//     double** slice_data,
-//     int* width, int* height,
-//     int* status
-// );
-
-// extern "C" API int map_fit(
-//     int map_id, int data_id,
-//     int* status
-// );
