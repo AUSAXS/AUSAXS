ab95860a8ea8698bb359809c9092181413926397
Kristian <kristian.lytje@uni-saarland.de>
2026-02-25 21:10:56 +0100

    refactored rigidbody interface

--- source/api/cli/cli_rigidbody.cpp
+++ source/api/cli/cli_rigidbody.cpp
@@ -31,30 +31,81 @@ int cli_rigidbody(int argc, char const *argv[]) {
     settings::grid::scaling = 2;
     settings::grid::cubic = true;
     settings::general::verbose = true;
+    bool save_settings = false;
 
-    CLI::App app{"[EXPERIMENTAL] Perform rigid-body optimization."};
     io::File pdb, mfile, settings, config;
     std::vector<unsigned int> constraints;
-    app.add_option("input_s", pdb, "Path to the structure file.")->required()->check(CLI::ExistingFile);
-    app.add_option("input_m", mfile, "Path to the measuremed data.")->check(CLI::ExistingFile);
-    app.add_option("output", settings::general::output, "Path to save the hydrated file at.")->default_val("output/rigidbody/");
-    auto p_cal = app.add_option("--calibrate", settings::rigidbody::detail::calibration_file, "Path to the calibration data.")->expected(0, 1)->check(CLI::ExistingFile);
-    app.add_option("--reduce,-r", settings::grid::water_scaling, "The desired number of water molecules as a percentage of the number of atoms. Use 0 for no reduction.");
-    app.add_option("--grid_width,-w", settings::grid::cell_width, "The distance between each grid point in Ångström (default: 1). Lower widths increase the precision.");
-    app.add_option_function<std::string>("--hydration_strategy,--hs", [] (const std::string& s) {settings::detail::parse_option("hydration_strategy", {s});}, "The placement strategy to use. Options: Radial, Axes, Jan.");
-    app.add_option("--qmin", settings::axes::qmin, "Lower limit on used q values from measurement file.");
-    app.add_option("--qmax", settings::axes::qmax, "Upper limit on used q values from measurement file.");
-    auto p_settings = app.add_option("-s,--settings", settings, "Path to the settings file.")->check(CLI::ExistingFile);
-    app.add_option("--iterations", settings::rigidbody::iterations, "Maximum number of iterations. Default: 1000.");
-    app.add_option("--constraints", settings::rigidbody::detail::constraints, "Constraints to apply to the rigid body.");
-    app.add_flag("--center,!--no-center", settings::molecule::center, "Decides whether the protein will be centered. Default: true.");
-    app.add_flag("--use-occupancy,!--no-use-occupancy", settings::molecule::use_occupancy, "Decides whether the atom occupancy will be taken into account when calculating the histogram. Default: false.");
-    app.add_flag("--allow-unknown-atoms", settings::molecule::allow_unknown_atoms, "Decides whether the program will quit if an unknown atom is found. Default: true.");
-    app.add_flag("--allow-unknown-residues", settings::molecule::allow_unknown_residues, "Decides whether the program will quit if an unknown residue is found. Default: true.");
+    CLI::App app{"[EXPERIMENTAL] Perform rigid-body optimization."};
+    app.fallthrough();
+    auto input_s = app.add_option("input_structure", pdb, "Path to the structure file.")->check(CLI::ExistingFile);
+    auto input_m = app.add_option("input_measurement", mfile, "Path to the measured SAXS data.")->check(CLI::ExistingFile);
+    app.add_option("--output,-o", settings::general::output, "Output folder to write the results to.")->default_val("output/rigidbody/")->group("General options");
+    app.add_flag_callback("--licence",    [] () {console::print_text(constants::licence); exit(0);}, "Print the licence.");
+    app.add_flag_callback("-v,--version", [] () {console::print_text(constants::version); exit(0);}, "Print the AUSAXS version.");
+    app.add_flag("--allow-unknown-atoms", settings::molecule::allow_unknown_atoms, 
+        "Allow processing files with unknown atoms. Use only if you understand the implications.")
+        ->default_val(settings::molecule::allow_unknown_atoms);
+    app.add_flag("--allow-unknown-residues", settings::molecule::allow_unknown_residues,
+        "Allow processing files with unknown residues. Use only if you understand the implications.")
+        ->default_val(settings::molecule::allow_unknown_residues);
+    app.add_flag("--offline", settings::general::offline, "Run the program in offline mode. This will prevent any network requests.")
+        ->default_val(settings::general::offline);
+    app.add_option("--threads,-t", settings::general::threads, "Number of threads to use.")->default_val(settings::general::threads);    
+
+    // config subcommands
+    auto sub_config = app.add_subcommand("config", "See and set additional options for the configuration.");
+    auto p_settings = sub_config->add_option("--file,-f", settings, "The configuration file to use.")->check(CLI::ExistingFile);
+    sub_config->add_flag("--save", save_settings, "Save the settings to a file.");
+    sub_config->add_flag_callback("--log", [] () {logging::start("saxs_fitter");}, "Enable logging to a file.");
+
+    // data subcommands
+    auto sub_data = app.add_subcommand("data", "See and set additional options for the SAXS data.");
+    sub_data->add_option(
+        "--qmax", 
+        settings::axes::qmax, 
+        "Upper limit on used q values from the measurement file.")
+        ->default_val(settings::axes::qmax)
+        ->check(CLI::Range(constants::axes::q_axis.min, constants::axes::q_axis.max))
+    ;
+    sub_data->add_option(
+        "--qmin", 
+        settings::axes::qmin, 
+        "Lower limit on used q values from the measurement file.")
+        ->default_val(settings::axes::qmin)
+        ->check(CLI::Range(constants::axes::q_axis.min, constants::axes::q_axis.max))
+    ;
+    sub_data->add_option_function<std::string>("--unit,-u", [] (const std::string& s) {settings::detail::parse_option("unit", {s});}, 
+        "The unit of the q values in the measurement file. Options: A, nm.");
+    sub_data->add_option("--skip", settings::axes::skip, "Number of points to skip in the measurement file.")->default_val(settings::axes::skip);
+    sub_data->add_flag("--rebin", settings::flags::data_rebin, "Rebin the data to increase the information content of each data point.")->default_val(settings::flags::data_rebin);
+
+    // molecule subcommands
+    auto sub_mol = app.add_subcommand("molecule", "See and set additional options for the molecular structure file.");
+    sub_mol->add_flag("--center,!--no-center", settings::molecule::center, 
+        "Decides whether the protein will be centered.")->default_val(settings::molecule::center);
+    sub_mol->add_flag("--use-occupancy,!--ignore-occupancy", settings::molecule::use_occupancy, 
+        "Decides whether the atomic occupancies from the file will be used.")->default_val(settings::molecule::use_occupancy);
+
+    // hydrogen subcommands
+    auto sub_hydrogen = app.add_subcommand("hydrogens", "See and set additional options for the handling of hydration atoms.");
+    sub_hydrogen->add_flag("--keep,!--discard", settings::general::keep_hydrogens, "Keep or discard hydrogens from the structure file.")->default_val(settings::general::keep_hydrogens);
+
+    // grid subcommands
+    auto sub_grid = app.add_subcommand("grid", "See and set additional options for the grid calculations.");
+    sub_grid->add_option("--width,-w", settings::grid::cell_width, 
+        "The distance between each grid point in Ångström. Lower widths increase the precision."
+    )->default_val(settings::grid::cell_width);
+
+    // hidden options group
+    app.add_flag("--weighted-bins", settings::hist::weighted_bins, 
+        "Decides whether the weighted bins will be used."
+    )->default_val(settings::hist::weighted_bins)->group("");
+
+    auto p_cal = app.add_option("--calibrate", settings::rigidbody::detail::calibration_file, "Path to the calibration data.")->expected(0, 1)->check(CLI::ExistingFile)->group("");
+    app.add_option("--constraints", settings::rigidbody::detail::constraints, "Constraints to apply to the rigid body.")->group("");
     CLI11_PARSE(app, argc, argv);
 
     console::print_info("Running AUSAXS " + std::string(constants::version));
-
     settings::rigidbody::constraint_generation_strategy = settings::rigidbody::ConstraintGenerationStrategyChoice::None;
     settings::flags::init_histogram_manager = false;
     logging::start("rigidbody");
