290a74967c72c5a3bc922455a55a706999bd9dc3
Krelle <kristian.lytje@uni-saarland.de>
2026-06-16 18:44:05 +0200

    more useful error message when the user mixes up pdb & dat files

--- source/core/constants/ValidFileExtensions.cpp
+++ source/core/constants/ValidFileExtensions.cpp
@@ -2,9 +2,10 @@
 // Author: Kristian Lytje
 
 #include <constants/ValidFileExtensions.h>
-
-#include <io/File.h>
 #include <utility/StringUtils.h>
+#include <io/File.h>
+
+#include <stdexcept>
 
 using namespace ausaxs;
 
@@ -18,6 +19,23 @@ bool constants::filetypes::detail::FileType<N>::check(const io::File& path) cons
         }
     }
     return false;
+}
+
+std::string constants::filetypes::detail::guess_type(const io::File& path) {
+    if (structure.check(path)) {
+        return "structure";
+    } if (saxs_data.check(path)) {
+        return "saxs_data";
+    } if (em_map.check(path)) {
+        return "em_map";
+    } if (unit_cell.check(path)) {
+        return "unit_cell";
+    } if (grid.check(path)) {
+        return "grid";
+    } if (config.check(path)) {
+        return "config";
+    }
+    return "";
 }
 
 template class constants::filetypes::detail::FileType<1>;
