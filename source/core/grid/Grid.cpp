02a49153baf57fc848ee671a994574185545a548
Krelle <kristian.lytje@uni-saarland.de>
2026-08-21 09:44:47 +0200

    fix failing test

--- source/core/grid/Grid.cpp
+++ source/core/grid/Grid.cpp
@@ -52,6 +52,7 @@ Grid::Grid(const std::vector<Body>& bodies) {
         }
         
         // Account for symmetry bodies by transforming bounding box corners
+        if (body.size_symmetry() == 0) {continue;}
         auto cm = body.get_cm();
         for (std::size_t j = 0; j < body.size_symmetry(); ++j) {
             auto sym = body.symmetry().get(j);
