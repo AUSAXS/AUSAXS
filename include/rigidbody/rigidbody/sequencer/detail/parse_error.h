508eb37d9c03bf64d179b15b4d753672e81168c5
Krelle <kristian.lytje@uni-saarland.de>
2026-08-22 09:18:08 +0200

    added ci check

--- include/rigidbody/rigidbody/sequencer/detail/parse_error.h
+++ include/rigidbody/rigidbody/sequencer/detail/parse_error.h
@@ -4,23 +4,23 @@
 #pragma once
 
 #include <rigidbody/sequencer/detail/ParsedArgs.h>
+#include <utility/Exceptions.h>
 
-#include <stdexcept>
 #include <string>
 #include <numeric>
 
 namespace ausaxs::rigidbody::sequencer::except {
-    struct parse_error : public std::invalid_argument {
-        explicit parse_error(std::string_view element, std::string_view msg) : std::invalid_argument(
+    struct parse_error : public ausaxs::except::base {
+        explicit parse_error(std::string_view element, std::string_view msg) : ausaxs::except::base(
             "Error parsing element \"" + std::string(element) + "\": " + std::string(msg)
         ) {}
 
-        explicit parse_error(std::string_view element, const ParsedArgs::Args::Arg& arg, std::string_view msg) : std::invalid_argument(
+        explicit parse_error(std::string_view element, const ParsedArgs::Args::Arg& arg, std::string_view msg) : ausaxs::except::base(
             "Error parsing element \"" + std::string(element) + "\" at line " + std::to_string(arg.line_number) + ": "
             "Invalid argument \"" + arg.str + "\".\n\t" + std::string(msg)
         ) {}
 
-        explicit parse_error(std::string_view element, const ParsedArgs::InlineArgs& args, std::string_view msg) : std::invalid_argument(
+        explicit parse_error(std::string_view element, const ParsedArgs::InlineArgs& args, std::string_view msg) : ausaxs::except::base(
             "Error parsing element \"" + std::string(element) + "\" at line " + std::to_string(args.line_number) + ". "
             "Invalid inline arguments: " + std::accumulate(args.values.begin(), args.values.end(), std::string(),
                 [] (const std::string& a, const std::string& b) {
