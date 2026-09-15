7d23d5a2cae7e947ba5d774cb7c6d99d40c81e32
Kristian <kristian.lytje@uni-saarland.de>
2026-09-15 22:50:19 +0200

    enabled lto & removed unused exes

--- executable/em_fitter.cpp
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
-// Author: Kristian Lytje
-
-#include <api/cli/cli_em_fitter.h>
-
-int main(int argc, char const *argv[]) {
-    return cli_em_fitter(argc, argv);
-}
--- executable/rigidbody_optimizer.cpp
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
-// Author: Kristian Lytje
-
-#include <api/cli/cli_rigidbody.h>
-
-int main(int argc, char const *argv[]) {
-    return cli_rigidbody(argc, argv);
-}
